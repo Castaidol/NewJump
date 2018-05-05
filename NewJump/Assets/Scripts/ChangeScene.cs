4ac0a5ff39b3d066de16ec74fe479a4752bae282
Castaidol <gabriele.castaway@gmail.com>
2018-05-05 16:39:39 +0200

    Ads
    add ADs and navigation between scenes

--- NewJump/Assets/Scripts/ChangeScene.cs
+++ NewJump/Assets/Scripts/ChangeScene.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public bool goToTitleScreen;
+    public bool goToCharSelect;
+    public bool goToWeaponSelect;
+    public bool goToLevelSelect;
+    public bool goToGame;
+
+    private int sceneIndex;
+
+	private void Start()
+	{
+        if (goToTitleScreen == true) sceneIndex = 0;
+        else if (goToCharSelect == true) sceneIndex = 1;
+        else if (goToWeaponSelect == true) sceneIndex = 2;
+        else if (goToLevelSelect == true) sceneIndex = 3;
+        else if (goToGame == true) sceneIndex = 4;
+        else sceneIndex = 1;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+	public void GoToScene()
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
 }
---
+++ NewJump/Assets/Scripts/PlayAdvertise.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class PlayAdvertise : MonoBehaviour {
+
+	void Awake ()
+    {
+        Advertisement.Initialize("1789890");
+	}
+	
+    public void ShowAds()
+    {
+        Advertisement.Show();
+    }
+}
