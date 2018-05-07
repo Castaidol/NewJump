fcb5906137078acada4e43d4f1f0ad5dbf02b1ab
Castaidol <gabriele.castaway@gmail.com>
2018-05-07 09:34:08 +0200

    .

---
+++ NewJump/Assets/Scripts/SelectCharcter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectCharcter : MonoBehaviour {
+
+    public bool isSelected;
+
+    Animator anim;
+	
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+        isSelected = false;
+	}
+	
+	
+	void Update ()
+    {
+		
+	}
+
+	private void OnMouseDown()
+	{
+        if(!isSelected)
+        {
+            isSelected = true;
+            anim.SetBool("isSelected", isSelected);
+            return;
+        }
+
+        if(isSelected)
+        {
+            isSelected = false;
+            anim.SetBool("isSelected", isSelected);
+            return;
+        }
+       
+	}
+}
